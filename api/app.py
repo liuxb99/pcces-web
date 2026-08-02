789fc8e313339baa97970c5066f40a02c0d3f7c6
liuxb99 <121744662+liuxb99@users.noreply.github.com>
2026-08-02 22:54:23 +0800

    feat(phase5): register contract allocation blueprint

--- api/app.py
+++ api/app.py
@@ -17,6 +17,7 @@ from api.budget_lock_guard import install_budget_lock_guard
 from api.budget_submission_gate import install_budget_submission_gate
 from api.budget_validation import BudgetValidationService, build_budget_validation_blueprint
 from api.budget_versioning import BudgetVersionService, build_budget_version_blueprint
+from api.contract_allocation import ContractAllocationService, build_contract_allocation_blueprint
 from api.contract_core import ContractCoreService, build_contract_core_blueprint
 from api.conversion_export_jobs import ConversionExportJobService, build_conversion_export_job_blueprint
 from api.conversion_long_jobs import ConversionLongJobService, build_conversion_long_job_blueprint
@@ -83,6 +84,7 @@ budget_cross_project_service = BudgetCrossProjectSyncService(engine)
 bid_lifecycle_service = BidLifecycleService(engine)
 budget_bid_conversion_service = BudgetBidConversionService(engine)
 contract_core_service = ContractCoreService(engine)
+contract_allocation_service = ContractAllocationService(engine)
 conversion_wizard_service = ConversionWizardService(engine)
 conversion_export_job_service = ConversionExportJobService(engine)
 conversion_long_job_service = ConversionLongJobService(engine)
@@ -165,6 +167,7 @@ register("budget_cross_project", build_budget_cross_project_blueprint(budget_cro
 register("bid_lifecycle", build_bid_lifecycle_blueprint(bid_lifecycle_service, resolve_user_id))
 register("budget_bid_conversion", build_budget_bid_conversion_blueprint(budget_bid_conversion_service, resolve_user_id))
 register("contract_core", build_contract_core_blueprint(contract_core_service, resolve_user_id))
+register("contract_allocation", build_contract_allocation_blueprint(contract_allocation_service, resolve_user_id))
 register("conversion_wizard", build_conversion_wizard_blueprint(conversion_wizard_service, resolve_user_id))
 register("conversion_export_jobs", build_conversion_export_job_blueprint(conversion_export_job_service, resolve_user_id))
 register("conversion_long_jobs", build_conversion_long_job_blueprint(conversion_long_job_service, resolve_user_id))
@@ -193,11 +196,11 @@ __all__ = [
     "app", "authorization_service", "work_context_service", "recovery_service", "persistence_service",
     "budget_decimal_service", "budget_version_service", "budget_approval_service", "budget_validation_service",
     "budget_cross_project_service", "bid_lifecycle_service", "budget_bid_conversion_service", "contract_core_service",
-    "conversion_wizard_service", "conversion_export_job_service", "conversion_long_job_service",
-    "legacy_exchange_adapter_service", "mrs_catalog_service", "mrs_code_service", "mrs_exchange_service",
-    "mrs_intelligence_service", "mrs_operations_service", "mrs_governance_service", "mrs_history_apply_service",
-    "resource_decimal_service", "resource_budget_lineage_service", "resource_budget_link_service",
-    "resource_dependency_graph_service", "budget_trace_service", "cost_structure_service",
-    "cost_structure_detail_service", "project_cost_structure_run_service", "cost_structure_run_version_service",
-    "resolve_user_id",
+    "contract_allocation_service", "conversion_wizard_service", "conversion_export_job_service",
+    "conversion_long_job_service", "legacy_exchange_adapter_service", "mrs_catalog_service", "mrs_code_service",
+    "mrs_exchange_service", "mrs_intelligence_service", "mrs_operations_service", "mrs_governance_service",
+    "mrs_history_apply_service", "resource_decimal_service", "resource_budget_lineage_service",
+    "resource_budget_link_service", "resource_dependency_graph_service", "budget_trace_service",
+    "cost_structure_service", "cost_structure_detail_service", "project_cost_structure_run_service",
+    "cost_structure_run_version_service", "resolve_user_id",
 ]
