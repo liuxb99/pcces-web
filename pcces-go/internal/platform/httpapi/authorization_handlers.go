369f3b0a48a946b0f8abaebf2ac420136042d2b1
liuxb99 <121744662+liuxb99@users.noreply.github.com>
2026-08-02 21:59:20 +0800

    feat(phase4): register combine-bid routes

--- pcces-go/internal/platform/httpapi/authorization_handlers.go
+++ pcces-go/internal/platform/httpapi/authorization_handlers.go
@@ -35,6 +35,7 @@ func (s *Server) authorizationRoutes() {
 	s.conversionExportJobRoutes()
 	s.bidImportRoundTripRoutes()
 	s.bidImportApplyRoutes()
+	s.combineBidRoutes()
 }
 
 func (s *Server) actorCapability(w http.ResponseWriter, r *http.Request) {
